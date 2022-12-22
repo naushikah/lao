dd237c6c8acfb1dfed26edca38acf67d3da4941e
Leslie Aparecida Oliveira <leslieapoliveira@gmail.com>
2022-12-22 13:54:30 -0300

    adjust page home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,64 @@
-import { Link } from "react-router-dom";
-import { 
+import { Col, Container, Row } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+import {
   BackgroundDark,
+  WelcomeContainer,
+  Heading1,
+  Span,
+  ImageStyled as Image,
+  MenuContainer,
+  ParagraphLight,
 } from "./styles";
+import Logo from "../../assets/images/logo-main.svg";
 
 const Home = () => {
   return (
     <>
-      <BackgroundDark/>
-      <Link to="/page2" className="App-link">
-          Next Page
-      </Link>
+      <BackgroundDark />
+      <Container>
+        <Row>
+          <Col
+            lg={4}
+            xxl={3}
+            className="d-flex justify-content-center justify-content-lg-end p-lg-0"
+          >
+            <Image src={Logo} />
+          </Col>
+          <Col className="p-lg-0">
+            <ParagraphLight className="text-center d-lg-none">
+              seja bem vindo(a) ao meu
+            </ParagraphLight>
+            <WelcomeContainer className="d-none d-lg-block">
+              <p>seja</p>
+              <p>bem vindo(a)</p>
+              <p>ao meu</p>
+            </WelcomeContainer>
+            <Heading1>
+              <Span>port</Span>fólio.
+            </Heading1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="p-lg-0" lg={{ offset: 4 }} xxl={{ offset: 3 }}>
+            <hr></hr>
+            <MenuContainer className="d-flex flex-column flex-sm-row justify-content-center">
+              <h2>
+                <Span>projetos</Span>
+              </h2>
+              <h2>sobre</h2>
+              <h2>contato</h2>
+            </MenuContainer>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Lesliê Aparecida Oliveira</h3>
+          </Col>
+        </Row>
+      </Container>
+      {/* <Link to="/page2" className="App-link">
+        Next Page
+      </Link> */}
     </>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,4 @@
+import { Image } from "react-bootstrap";
 import styled from "styled-components";
 
 export const BackgroundDark = styled.div`
@@ -7,5 +8,82 @@ export const BackgroundDark = styled.div`
   left: 0;
   width: 50%;
   height: 100%;
-  z-index: -1; 
+  z-index: -1;
+
+  @media (max-width: 991px) {
+    width: 100%;
+    height: 50%;
+  }
+`;
+
+export const WelcomeContainer = styled.div`
+  font-size: 20px;
+  font-weight: 300;
+  color: #ffffff;
+
+  p {
+    margin: 0;
+  }
+`;
+
+export const Heading1 = styled.h1`
+  color: #000000;
+  letter-spacing: 5px;
+
+  @media (max-width: 991px) {
+    color: #ffffff;
+    text-align: center;
+    font-size: 50px;
+  }
+
+  @media (min-width: 992px) and (max-width: 1199px) {
+    font-size: 69px !important;
+  }
+
+  @media (min-width: 1200px) and (max-width: 1399px) {
+    font-size: 84px;
+  }
+
+  @media (min-width: 1400px) {
+    font-size: 152px;
+  }
+`;
+
+export const ParagraphLight = styled.p`
+  color: #ffffff;
+  font-weight: 200;
+  font-size: 20px;
+`;
+
+export const Span = styled.span`
+  color: #ffffff;
+`;
+
+export const ImageStyled = styled(Image)`
+  @media (max-width: 991px) {
+    width: 200px;
+  }
+
+  @media (min-width: 992px) and (max-width: 1199px) {
+    width: 300px;
+  }
+`;
+
+export const MenuContainer = styled.div`
+  h2 {
+    font-weight: 200;
+    letter-spacing: 1px;
+  }
+
+  @media (min-width: 576px) and (max-width: 991px) {
+    gap: 5rem;
+  }
+
+  @media (min-width: 992px) and (max-width: 1199px) {
+    gap: 8rem;
+  }
+
+  @media (min-width: 1200px) {
+    gap: 10rem;
+  }
 `;
