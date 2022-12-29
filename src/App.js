346ae3576c2221944c91c9fa500784ab497520c9
Leslie Aparecida Oliveira <leslieapoliveira@gmail.com>
2022-12-29 18:30:39 -0300

    remove redirect 404 page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Navbar } from "./components";
 import { Global, FooterStyled as Footer } from "./global.css";
-import { About, Contact, Home, NotFound, Projects } from "./pages";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => (
   <BrowserRouter>
@@ -14,8 +14,6 @@ const App = () => (
         <Route path="/projetos" exact component={Projects} />
         <Route path="/sobre" exact component={About} />
         <Route path="/contato" exact component={Contact} />
-        <Route path="/404" component={NotFound} />
-        <Redirect from="*" to="/404" />
       </Switch>
       <Footer />
     </main>
