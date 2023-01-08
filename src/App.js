ed5221c929f2da06f8233d9ebfad6e5ec893f4bf
Leslie Aparecida Oliveira <leslieapoliveira@gmail.com>
2023-01-08 13:19:02 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-// import { Navbar } from "./components";
 import { Global, FooterStyled as Footer, NavbarStyled as Navbar } from "./global.css";
-import { About, Contact, Home, NotFound, Projects } from "./pages";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => (
   <BrowserRouter>
@@ -14,7 +13,6 @@ const App = () => (
         <Route path="/projetos" exact component={Projects} />
         <Route path="/sobre" exact component={About} />
         <Route path="/contato" exact component={Contact} />
-        <Route path="/404" component={NotFound} />
       </Switch>
       <Footer />
     </main>
